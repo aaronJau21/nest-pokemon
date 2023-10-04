53bb7fa4df420d977f9a3f294761a6b9b76516c0
aaronJau21 <aaronjau21@gmail.com>
2023-10-03 23:01:37 -0500

    seed sacado del PokeApi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     PokemonModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/pokemon'),
     CommonModule,
+    SeedModule,
   ],
 })
 export class AppModule {}
